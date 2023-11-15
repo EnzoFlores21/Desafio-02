ecf8d5df4989fb4ad9ed39d9eada323841c088ab
enzoflores <enzoflores15k@gmail.com>
2023-11-15 09:55:27 -0300

    re-entrega

--- productManager.js
+++ productManager.js
@@ -1,9 +1,8 @@
 const fs = require("node:fs")
 
-// Clase para manejar posts
+
 class ProductManager {
 
-    // Constructor para leer y parsear los productos
     constructor(fileName) {
         this.path = fileName
         this.id = 0
@@ -19,7 +18,7 @@ class ProductManager {
         }
     }
 
-    async saveFile(){
+    async saveFile() {
         try {
             await fs.promises.writeFile(
                 this.path,
@@ -31,29 +30,47 @@ class ProductManager {
         }
     }
 
-    getProducts(){
+    getProducts() {
         console.log(this.products)
         return this.products
     }
 
-    async addProduct(product){
+    async addProduct(product) {
         const buscador = this.products.find((p) => p.code === product.code)
 
         if (buscador) {
             console.log("El codigo del producto ya existe");
         } else {
-            const newProduct = {...product, id: this.products.length + 1}
+            const newProduct = { ...product, id: this.products.length + 1 }
             this.products.push(newProduct)
 
             await this.saveFile()
         }
     }
 
-    async deleteProduct(id){
+    async updateProduct(id, fieldsToUpdate) {
+
+        const productoIndex = this.products.findIndex((producto) => producto.id === id);
+        console.log(productoIndex);
+        if (productoIndex === -1) {
+            return { mensaje: 'Producto no encontrado' };
+        }
+
+
+        this.products[productoIndex] = {
+            ...products[productoIndex],
+            ...fieldsToUpdate,          
+            id: products[productoIndex].id,
+        };
+
+        return { mensaje: 'Producto actualizado correctamente', products: this.products[productoIndex] };
+    }
+
+    async deleteProduct(id) {
         const selector = this.products.find((p) => p.id === id)
 
         if (selector) {
-            const newProductArray = this.products.filter((p) => p.id =! id)
+            const newProductArray = this.products.filter((p) => p.id != id)
 
             this.products = newProductArray
 
@@ -94,6 +111,10 @@ async function PruebaProducts() {
     productManager.getProducts();
 
     await productManager.deleteProduct(1);
+
+    productManager.getProducts();
+
+    await productManager.updateProduct(new Product("Producto3", "PC", 1200, "dasda", 2, 40));
 
     productManager.getProducts();
 }
